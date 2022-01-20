1acb683af0a8e47b038c3ce0ba797a3241ae260b
webethics <info@webethicssolutions.com>
2022-01-20 20:01:30 +0530

    update the homepage

--- src/components/HomePro/Services.vue
+++ src/components/HomePro/Services.vue
@@ -1,67 +1,12 @@
 <template>
   <div class="services">
-    <div class="container mx-auto px-5 py-8 sm:py-12 lg:py-20 3xl:py-24">
-      <div
-        class="
-          flex flex-col
-          md:flex-row
-          items-stretch
-          justify-between
-          space-x-0
-          md:space-x-5
-          lg:space-x-8
-        "
-      >
+    <div class="container mx-auto px-5 lg:px-20 2xl:px-28 3xl:px-5 py-8 sm:py-12 lg:py-20 3xl:py-24">
+      <div class="flex flex-col md:flex-row items-stretch justify-between space-x-0 md:space-x-5 lg:space-x-8">
         <div
-          class="
-            services-wrap
-            rounded-md
-            text-center
-            md:text-left
-            w-full
-            relative
-            mb-5
-            overflow-hidden
-          "
-        >
-          <div
-            class="
-              services-img
-              bg-plumber bg-cover bg-bottom
-              transition-transform
-              duration-500
-              ease-in-out
-            "
-          ></div>
-          <div
-            class="
-              services-meta-wrap
-              rounded-b-md
-              px-6
-              py-4
-              md:py-6
-              flex flex-col
-              md:flex-row
-              justify-between
-              items-center
-              absolute
-              bottom-0
-              left-0
-              right-0
-              mx-auto
-            "
-          >
-            <h3
-              class="
-                text-lg
-                sm:text-xl
-                lg:text-2xl
-                xl:text-six
-                3xl:text-five
-                font-bold
-                text-white
-              "
-            >
+          class="services-wrap rounded-md text-center md:text-left w-full relative mb-5 overflow-hidden scale-100">
+          <div class="services-img bg-plumber bg-cover bg-bottom transition-transform duration-500 ease-in-out"></div>
+          <div class="services-meta-wrap rounded-b-md px-6 py-4 md:py-6 flex flex-col md:flex-row justify-between items-center absolute bottom-0 left-0 right-0 mx-auto">
+            <h3 class="text-lg sm:text-xl lg:text-2xl xl:text-six 3xl:text-five font-bold text-white">
               <a href="#"> Plumber</a>
             </h3>
             <p class="text-sm sm:text-base lg:text-lg text-white">
@@ -70,56 +15,10 @@
           </div>
         </div>
 
-        <div
-          class="
-            services-wrap
-            rounded-md
-            text-center
-            md:text-left
-            w-full
-            relative
-            mb-5
-            overflow-hidden
-          "
-        >
-          <div
-            class="
-              services-img
-              bg-contractor bg-cover bg-bottom
-              transition-transform
-              duration-500
-              ease-in-out
-            "
-          ></div>
-          <div
-            class="
-              services-meta-wrap
-              rounded-b-md
-              px-6
-              py-4
-              md:py-6
-              flex flex-col
-              md:flex-row
-              justify-between
-              items-center
-              absolute
-              bottom-0
-              left-0
-              right-0
-              mx-auto
-            "
-          >
-            <h3
-              class="
-                text-lg
-                sm:text-xl
-                lg:text-2xl
-                xl:text-six
-                3xl:text-five
-                font-bold
-                text-white
-              "
-            >
+        <div class="services-wrap rounded-md text-center md:text-left w-full relative mb-5 overflow-hidden scale-100">
+          <div class="services-img bg-contractor bg-cover bg-bottom transition-transform duration-500 ease-in-out"></div>
+          <div class="services-meta-wrap rounded-b-md px-6 py-4 md:py-6 flex flex-col md:flex-row justify-between items-center absolute bottom-0 left-0 right-0 mx-auto">
+            <h3 class="text-lg sm:text-xl lg:text-2xl xl:text-six 3xl:text-five font-bold text-white">
               <a href="#"> Contractor</a>
             </h3>
             <p class="text-sm sm:text-base lg:text-lg text-white">
@@ -129,65 +28,11 @@
         </div>
       </div>
 
-      <div
-        class="
-          flex flex-col
-          md:flex-row
-          items-stretch
-          justify-between
-          space-x-0
-          md:space-x-5          
-        "
-      >
-        <div
-          class="
-            services-wrap
-            rounded-md
-            text-center
-            md:text-left
-            w-full
-            relative
-            overflow-hidden
-          "
-        >
-          <div
-            class="
-              services-img
-              bg-realtor bg-cover bg-bottom
-              transition-transform
-              duration-500
-              ease-in-out
-            "
-          ></div>
-          <div
-            class="
-              services-meta-wrap
-              rounded-b-md
-              px-6
-              py-4
-              md:py-6
-              flex flex-col
-              md:flex-row
-              justify-between
-              items-center
-              absolute
-              bottom-0
-              left-0
-              right-0
-              mx-auto
-            "
-          >
-            <h3
-              class="
-                text-lg
-                sm:text-xl
-                lg:text-2xl
-                xl:text-six
-                3xl:text-five
-                font-bold
-                text-white
-              "
-            >
+      <div class="flex flex-col md:flex-row items-stretch justify-between space-x-0 md:space-x-5">
+        <div class="services-wrap rounded-md text-center md:text-left w-full relative overflow-hidden scale-100">
+          <div class="services-img bg-realtor bg-cover bg-bottom transition-transform duration-500 ease-in-out"></div>
+          <div class="services-meta-wrap rounded-b-md px-6 py-4 md:py-6 flex flex-col md:flex-row justify-between items-center absolute bottom-0 left-0 right-0 mx-auto">
+            <h3 class="text-lg sm:text-xl lg:text-2xl xl:text-six 3xl:text-five font-bold text-white">
               <a href="#">Realtor</a>
             </h3>
             <p class="text-sm sm:text-base lg:text-lg text-white">
